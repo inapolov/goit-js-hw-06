2e7d6c6cb3a10762d4c18269989384f1a5d6d65a
Igor Napolov <isnapolov@gmail.com>
2021-11-03 17:50:56 +0200

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ingredients.forEach(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient; 
+  item.classList.add('item'); 
+
+const list=document.querySelector('ul#ingredients')
+  list.appendChild(item);
+})
+
