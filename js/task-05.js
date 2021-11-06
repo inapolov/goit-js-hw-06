4809f47096f8343fed78c45bbc4ebaeffd59553d
Igor Napolov <isnapolov@gmail.com>
2021-11-06 10:46:41 +0200

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const nameInputEl = document.querySelector('#name-input');
+const nameOutputEl = document.querySelector('#name-output');
+
+nameInputEl.addEventListener("input", (event) => {
+     nameOutputEl.textContent = event.currentTarget.value;
+    
+})
