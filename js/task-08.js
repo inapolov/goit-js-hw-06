eef5eed946e7852aca883775ea9c48b1842faedf
Igor Napolov <isnapolov@gmail.com>
2021-11-06 14:59:34 +0200

    Task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginFormEl = document.querySelector(".login-form");
 
+loginFormEl.addEventListener("submit", handleSubmit);
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email === "" || password === "") {
+    alert("All fields must be filled!");
+    } else {
+     const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData);
+    
+    event.currentTarget.reset();   
+}
+    
+}
