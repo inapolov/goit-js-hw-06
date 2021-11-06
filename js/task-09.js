791b39a9ad3d04f8ea9e67a9b877ee3d36d854af
Igor Napolov <isnapolov@gmail.com>
2021-11-06 15:14:50 +0200

    Task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const changeColorEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+
+changeColorEl.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  colorEl.textContent = `${getRandomHexColor()}`;
+})
